34dc5bb2e92bbbbbf5aa2badd224b6c943812e0a
katilio <adanrodriguez@gmail.com>
2019-12-02 13:53:12 +0100

    Add files via upload

---
+++ Advent2019/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2019
+{
+
+    public class Shared
+    {
+        public static string[] getInput(string filename)
+        {
+            string path = "C:/Users/arodriguezcuadra/Downloads/AoC2019/" + filename;
+            string[] input = File.ReadAllLines(@path).Select(s => s.TrimStart()).ToArray();
+            return input;
+        }
+        public static List<string> getInputList(string filename)
+        {
+            List<string> input = File.ReadAllLines(@filename).Select(s => s.TrimStart()).ToList<string>();
+            return input;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Shared.getInput("day1.txt");
+            double totalFuel = 0;
+            foreach (string line in input)
+            {
+                double dividedMass = (int.Parse(line) / 3);
+                double fuel = Math.Floor(dividedMass) - 2;
+                totalFuel += fuel;
+                while (fuel > 6) // Part 2
+                {
+                    fuel = fuel / 3;
+                    fuel = Math.Floor(fuel) - 2;
+                    totalFuel += fuel;
+                }
+            }
+
+            Console.WriteLine(totalFuel);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/arodriguezcuadra/Downloads/AoC2019/Day2.txt";
+
+            string input = File.ReadAllLines(@path).Select(s => s.TrimStart(',')).ToArray()[0];
+            var opcode = input.Split(',').Select(Int32.Parse).ToList();
+            opcode[1] = 12;
+            opcode[2] = 2;
+
+            int cursor = 0;
+            while (cursor < opcode.Count() - 3)
+            {
+                switch (opcode[cursor])
+                {
+                    case 1:
+                        opcode[opcode[cursor + 3]] = opcode[opcode[cursor + 1]] + opcode[opcode[cursor + 2]];
+                        cursor += 4;
+                        break;
+                    case 2:
+                        opcode[opcode[cursor + 3]] = opcode[opcode[cursor + 1]] * opcode[opcode[cursor + 2]];
+                        cursor += 4;
+                        break;
+                    case 99:
+                        Console.WriteLine(opcode[0]);
+                        Console.ReadLine();
+                        break;
+
+                }
+            }
+        }
+
+    }
+}
